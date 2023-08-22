36d7f347c458007eb9708a45a1213d9f417bf2c5
Danny Herrmann <dannyherrmann@Dannys-iMac.local>
2023-08-21 21:35:58 -0500

    Initial Commit

---
+++ LibrespotObjC-lib/LibrespotObjC_lib.h
@@ -0,0 +1,13 @@
+//
+//  LibrespotObjC_lib.h
+//  LibrespotObjC-lib
+//
+//  Created by Danny Herrmann on 8/21/23.
+//  Copyright (c) 2023 Danny Herrmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibrespotObjC_lib : NSObject
+
+@end
---
+++ LibrespotObjC-lib/LibrespotObjC_lib.m
@@ -0,0 +1,13 @@
+//
+//  LibrespotObjC_lib.m
+//  LibrespotObjC-lib
+//
+//  Created by Danny Herrmann on 8/21/23.
+//  Copyright (c) 2023 Danny Herrmann. All rights reserved.
+//
+
+#import "LibrespotObjC_lib.h"
+
+@implementation LibrespotObjC_lib
+
+@end
